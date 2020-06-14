9af667f4451f2dd56d7df8be54453c262871db34
horaanchal <horaanchal@gmail.com>
2020-06-14 18:54:46 +0530

    extra imports removed

--- arms-services/src/Arms.Api/Controllers/ARMSEmployeeRolesController.cs
+++ arms-services/src/Arms.Api/Controllers/ARMSEmployeeRolesController.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Arms.Application.Services.Users;
-using Microsoft.AspNetCore.Http;
 using Arms.Infrastructure;
 using Arms.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
 using Arms.Domain.CustomEntities;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
--- arms-services/src/Arms.Infrastructure/EntityTypeConfigurations/LocationEntityTypeConfiguration.cs
+++ arms-services/src/Arms.Infrastructure/EntityTypeConfigurations/LocationEntityTypeConfiguration.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Arms.Domain.Entities;
+﻿using Arms.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
